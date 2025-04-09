8d2b56d17d12298329c8b04ec6eaa1157e7efeaf
Abdullah Mumtaz Fayyadh <mumtazfayyadh@gmail.com>
2025-04-09 19:38:48 +0700

    About Screen

--- app/src/main/java/com/mumtazfayyadh0102/iformula/screen/AboutScreen.kt
+++ app/src/main/java/com/mumtazfayyadh0102/iformula/screen/AboutScreen.kt
@@ -1,24 +1,43 @@
 package com.mumtazfayyadh0102.iformula.screen
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.mumtazfayyadh0102.iformula.R
@@ -29,6 +48,7 @@ import com.mumtazfayyadh0102.iformula.ui.theme.LightF1Red
 @Composable
 fun AboutScreen(navController: NavController) {
     val appBarColor = if (isSystemInDarkTheme()) DarkF1Black else LightF1Red
+    val context = LocalContext.current
 
     Scaffold(
         topBar = {
@@ -66,9 +86,111 @@ fun AboutScreen(navController: NavController) {
             modifier = Modifier
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
-                .padding(16.dp)
+                .padding(16.dp),
         ) {
-            // Konten
+
+            Text(
+                text = stringResource(id = R.string.about_welcome),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
+        ) {
+
+            Text(
+                text = stringResource(id = R.string.about_welcome),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.dev_photo),
+                    contentDescription = "Developer Photo",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .clip(CircleShape)
+                        .border(1.dp, if (isSystemInDarkTheme()) Color.White else Color.Transparent, CircleShape)
+                )
+
+                Spacer(modifier = Modifier.padding(8.dp))
+
+                Text(
+                    text = stringResource(id = R.string.trakteer),
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                Button(
+                    onClick = {
+                        val uri = Uri.parse("https://trakteer.id/lamusiuu/tip")
+                        val intent = Intent(Intent.ACTION_VIEW, uri)
+                        context.startActivity(intent)
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if (isSystemInDarkTheme()) Color.Transparent else LightF1Red
+                    ),
+                    border = BorderStroke(
+                        width = 0.5.dp,
+                        color = if (isSystemInDarkTheme()) Color.White else Color.Transparent
+                    ),
+                    modifier = Modifier.padding(vertical = 8.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.trakteer_kopi),
+                        color = Color.White
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "iFormula © 2025",
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(id = R.string.all_rights_reserved),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = "lamusiuu®",
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(id = R.string.copyright_text),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = stringResource(id = R.string.all_rights_reserved),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
+            Text(
+                text = stringResource(id = R.string.license_text),
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
